7327f366443c91dce7745ba0786cbd3c6b853ef8
stump26 <moidpg@gmail.com>
2019-11-08 16:16:42 +0900

    implementing edittor

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOMServer from 'react-dom/server';
+import * as Showdown from 'showdown';
+import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
+
+const converter = new Showdown.Converter({
+  tables: true,
+  simplifiedAutoLink: true,
+  strikethrough: true,
+  tasklists: true,
+});
 
 const Editor = () => {
-  return <div>editor</div>;
+  const [value, setValue] = useState('#Hello World');
+  return (
+    <div>
+      <SimpleMDE
+        value={value}
+        onChange={setValue}
+        options={{
+          previewRender(text) {
+            return ReactDOMServer.renderToString(
+              Promise.resolve(converter.makeHtml(text)),
+            );
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default Editor;
