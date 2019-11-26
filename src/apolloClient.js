0856afd5326a12973b4fb8f57c83248fcb5d273d
stump26 <moidpg@gmail.com>
2019-11-26 17:27:37 +0900

    Add Grpahql authorize context

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,11 +1,42 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createUploadLink } from 'apollo-upload-client';
+import { setContext } from 'apollo-link-context';
 
-let uri = 'http://localhost:4000/graphql';
+const uri = process.env.REACT_APP_BACKEND_HOST + '/graphql';
+console.log('TCL: uri', uri);
+
+const httpLink = new HttpLink({
+	uri,
+});
+
+const authLink = setContext((_, { headers, ...context }) => {
+	// get the authentication token from local storage if it exists
+	const token = sessionStorage.getItem('token');
+	console.log('TCL: authLink -> token', token);
+	// return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+		...context,
+	};
+});
+const link = ApolloLink.from([
+	authLink,
+	httpLink,
+	createUploadLink({
+		uri,
+	}),
+]);
+
 const client = new ApolloClient({
-  uri,
-  link: ApolloLink.from([createUploadLink({ uri })]),
+	uri,
+	link: link,
+	cache: new InMemoryCache(),
 });
 
 export default client;
